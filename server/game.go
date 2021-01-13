5e84c4467eb9a20e5ee3fb6a4569f4986c06ad28
RoelofRuis <roelof.ruis@gmail.com>
2021-01-13 19:26:01 +0100

    implemented tiles

--- server/game.go
+++ server/game.go
@@ -1,16 +1,9 @@
 package main
 
-var TILESET = []Tile{
-	{Name: "bamboo 1"},
-	{Name: "bamboo 2"},
-	{Name: "bamboo 3"},
-}
-
 func InitGame(id uint64) Game {
 	players := make(map[int]Player, 4)
 
-	tiles := make([]Tile, len(TILESET))
-	copy(tiles, TILESET)
+	tiles := NewTileCollection()
 
 	return Game{
 		Id: id,
@@ -18,4 +11,55 @@ func InitGame(id uint64) Game {
 		Wall: tiles,
 		Players: players,
 	}
+}
+
+func NewTileCollection() TileCollection {
+	c := TileCollection{Tiles: make(map[Tile]int)}
+	c.Add(Bamboo1, 4)
+	c.Add(Bamboo2, 4)
+	c.Add(Bamboo3, 4)
+	c.Add(Bamboo4, 4)
+	c.Add(Bamboo5, 4)
+	c.Add(Bamboo6, 4)
+	c.Add(Bamboo7, 4)
+	c.Add(Bamboo8, 4)
+	c.Add(Bamboo9, 4)
+	c.Add(Circles1, 4)
+	c.Add(Circles2, 4)
+	c.Add(Circles3, 4)
+	c.Add(Circles4, 4)
+	c.Add(Circles5, 4)
+	c.Add(Circles6, 4)
+	c.Add(Circles7, 4)
+	c.Add(Circles8, 4)
+	c.Add(Circles9, 4)
+	c.Add(Characters1, 4)
+	c.Add(Characters2, 4)
+	c.Add(Characters3, 4)
+	c.Add(Characters4, 4)
+	c.Add(Characters5, 4)
+	c.Add(Characters6, 4)
+	c.Add(Characters7, 4)
+	c.Add(Characters8, 4)
+	c.Add(Characters9, 4)
+	c.Add(RedDragon, 4)
+	c.Add(GreenDragon, 4)
+	c.Add(WhiteDragon, 4)
+	c.Add(EastWind, 4)
+	c.Add(SouthWind, 4)
+	c.Add(NorthWind, 4)
+	c.Add(WestWind, 4)
+	c.Add(FlowerPlumb, 1)
+	c.Add(FlowerOrchid, 1)
+	c.Add(FlowerChrysanthemum, 1)
+	c.Add(FlowerBamboo, 1)
+	c.Add(SeasonSpring, 1)
+	c.Add(SeasonSummer, 1)
+	c.Add(SeasonAutumn, 1)
+	c.Add(SeasonWinter, 1)
+	return c
+}
+
+func (t *TileCollection) Add(tile Tile, n int) {
+	t.Tiles[tile] = n
 }
--- server/model.go
+++ server/model.go
@@ -1,8 +1,50 @@
 package main
 
-type Tile struct {
-	Name string
-}
+type Tile int
+const (
+	Bamboo1 Tile = 1
+	Bamboo2 Tile = 2
+	Bamboo3 Tile = 3
+	Bamboo4 Tile = 4
+	Bamboo5 Tile = 5
+	Bamboo6 Tile = 6
+	Bamboo7 Tile = 7
+	Bamboo8 Tile = 8
+	Bamboo9 Tile = 9
+	Circles1 Tile = 11
+	Circles2 Tile = 12
+	Circles3 Tile = 13
+	Circles4 Tile = 14
+	Circles5 Tile = 15
+	Circles6 Tile = 16
+	Circles7 Tile = 17
+	Circles8 Tile = 18
+	Circles9 Tile = 19
+	Characters1 Tile = 21
+	Characters2 Tile = 22
+	Characters3 Tile = 23
+	Characters4 Tile = 24
+	Characters5 Tile = 25
+	Characters6 Tile = 26
+	Characters7 Tile = 27
+	Characters8 Tile = 28
+	Characters9 Tile = 29
+	RedDragon Tile = 30
+	GreenDragon Tile = 31
+	WhiteDragon Tile = 32
+	EastWind Tile = 40
+	WestWind Tile = 41
+	SouthWind Tile = 42
+	NorthWind Tile = 43
+	FlowerPlumb Tile = 50
+	FlowerOrchid Tile = 51
+	FlowerChrysanthemum Tile = 52
+	FlowerBamboo Tile = 53
+	SeasonSpring Tile = 60
+	SeasonSummer Tile = 61
+	SeasonAutumn Tile = 62
+	SeasonWinter Tile = 63
+)
 
 type Combination struct {
 	Tiles []Tile
@@ -21,6 +63,10 @@ type Game struct {
 
 	HasEnded bool
 
-	Wall []Tile
+	Wall TileCollection
 	Players map[int]Player
+}
+
+type TileCollection struct {
+	Tiles map[Tile]int
 }
