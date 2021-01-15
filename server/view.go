bf08fd7faaa67085c5556e16389fa97a4f1b6591
RoelofRuis <roelof.ruis@gmail.com>
2021-01-15 19:33:00 +0100

    let tile descriptors be leading in list generation

--- server/view.go
+++ server/view.go
@@ -5,53 +5,53 @@ import (
 )
 
 type TileDescriptor struct {
+	Tile Tile
 	Name string
-	Pos int
 }
 
-var TileDescriptors = map[Tile]TileDescriptor {
-	Bamboo1: {Name: "Bamboo 1", Pos: 0},
-	Bamboo2: {Name: "Bamboo 2", Pos: 1},
-	Bamboo3: {Name: "Bamboo 3", Pos: 2},
-	Bamboo4: {Name: "Bamboo 4", Pos: 3},
-	Bamboo5: {Name: "Bamboo 5", Pos: 4},
-	Bamboo6: {Name: "Bamboo 6", Pos: 5},
-	Bamboo7: {Name: "Bamboo 7", Pos: 6},
-	Bamboo8: {Name: "Bamboo 8", Pos: 7},
-	Bamboo9: {Name: "Bamboo 1", Pos: 8},
-	Circles1: {Name: "Circles 1", Pos: 9},
-	Circles2: {Name: "Circles 2", Pos: 10},
-	Circles3: {Name: "Circles 3", Pos: 11},
-	Circles4: {Name: "Circles 4", Pos: 12},
-	Circles5: {Name: "Circles 5", Pos: 13},
-	Circles6: {Name: "Circles 6", Pos: 14},
-	Circles7: {Name: "Circles 7", Pos: 15},
-	Circles8: {Name: "Circles 8", Pos: 16},
-	Circles9: {Name: "Circles 9", Pos: 17},
-	Characters1: {Name: "Characters 1", Pos: 18},
-	Characters2: {Name: "Characters 2", Pos: 19},
-	Characters3: {Name: "Characters 3", Pos: 20},
-	Characters4: {Name: "Characters 4", Pos: 21},
-	Characters5: {Name: "Characters 5", Pos: 22},
-	Characters6: {Name: "Characters 6", Pos: 23},
-	Characters7: {Name: "Characters 7", Pos: 24},
-	Characters8: {Name: "Characters 8", Pos: 25},
-	Characters9: {Name: "Characters 9", Pos: 26},
-	RedDragon: {Name: "Red Dragon", Pos: 27},
-	GreenDragon: {Name: "Green Dragon", Pos: 28},
-	WhiteDragon: {Name: "White Dragon", Pos: 29},
-	EastWind: {Name: "East Wind", Pos: 30},
-	SouthWind: {Name: "South Wind", Pos: 31},
-	WestWind: {Name: "West Wind", Pos: 32},
-	NorthWind: {Name: "North Wind", Pos: 33},
-	FlowerPlumb: {Name: "Plumb (flower)", Pos: 34},
-	FlowerOrchid: {Name: "Orchid (flower)", Pos: 35},
-	FlowerChrysanthemum: {Name: "Chrysanthemum (flower)", Pos: 36},
-	FlowerBamboo: {Name: "Bamboo (flower)", Pos: 37},
-	SeasonSpring: {Name: "Spring (season)", Pos: 38},
-	SeasonSummer: {Name: "Summer (season)", Pos: 39},
-	SeasonAutumn: {Name: "Autumn (season)", Pos: 40},
-	SeasonWinter: {Name: "Winter (season)", Pos: 41},
+var TileDescriptors = []TileDescriptor {
+	{Tile: Bamboo1, Name: "Bamboo 1"},
+	{Tile: Bamboo2, Name: "Bamboo 2"},
+	{Tile: Bamboo3, Name: "Bamboo 3"},
+	{Tile: Bamboo4, Name: "Bamboo 4"},
+	{Tile: Bamboo5, Name: "Bamboo 5"},
+	{Tile: Bamboo6, Name: "Bamboo 6"},
+	{Tile: Bamboo7, Name: "Bamboo 7"},
+	{Tile: Bamboo8, Name: "Bamboo 8"},
+	{Tile: Bamboo9, Name: "Bamboo 1"},
+	{Tile: Circles1, Name: "Circles 1"},
+	{Tile: Circles2, Name: "Circles 2"},
+	{Tile: Circles3, Name: "Circles 3"},
+	{Tile: Circles4, Name: "Circles 4"},
+	{Tile: Circles5, Name: "Circles 5"},
+	{Tile: Circles6, Name: "Circles 6"},
+	{Tile: Circles7, Name: "Circles 7"},
+	{Tile: Circles8, Name: "Circles 8"},
+	{Tile: Circles9, Name: "Circles 9"},
+	{Tile: Characters1, Name: "Characters 1"},
+	{Tile: Characters2, Name: "Characters 2"},
+	{Tile: Characters3, Name: "Characters 3"},
+	{Tile: Characters4, Name: "Characters 4"},
+	{Tile: Characters5, Name: "Characters 5"},
+	{Tile: Characters6, Name: "Characters 6"},
+	{Tile: Characters7, Name: "Characters 7"},
+	{Tile: Characters8, Name: "Characters 8"},
+	{Tile: Characters9, Name: "Characters 9"},
+	{Tile: RedDragon, Name: "Red Dragon"},
+	{Tile: GreenDragon, Name: "Green Dragon"},
+	{Tile: WhiteDragon, Name: "White Dragon"},
+	{Tile: EastWind, Name: "East Wind"},
+	{Tile: SouthWind, Name: "South Wind"},
+	{Tile: WestWind, Name: "West Wind"},
+	{Tile: NorthWind, Name: "North Wind"},
+	{Tile: FlowerPlumb, Name: "Plumb (flower)"},
+	{Tile: FlowerOrchid, Name: "Orchid (flower)"},
+	{Tile: FlowerChrysanthemum, Name: "Chrysanthemum (flower)"},
+	{Tile: FlowerBamboo, Name: "Bamboo (flower)"},
+	{Tile: SeasonSpring, Name: "Spring (season)"},
+	{Tile: SeasonSummer, Name: "Summer (season)"},
+	{Tile: SeasonAutumn, Name: "Autumn (season)"},
+	{Tile: SeasonWinter, Name: "Winter (season)"},
 }
 
 type GameView struct {
@@ -70,11 +70,11 @@ func (g *Game) View() *GameView {
 
 func (t *TileCollection) Describe() []string {
 	var descriptions []string
-	for tile, count := range t.Tiles {
-		if count == 0 {
+	for _, d := range TileDescriptors {
+		count, has := t.Tiles[d.Tile]
+		if !has || count == 0 {
 			continue
 		}
-		d, _ := TileDescriptors[tile]
 		text := fmt.Sprintf("%d× %s", count, d.Name)
 		descriptions = append(descriptions, text)
 	}
@@ -82,10 +82,13 @@ func (t *TileCollection) Describe() []string {
 }
 
 func (t *TileCollection) ToVector() []int {
-	tileVector := make([]int, 42)
-	for tile, count := range t.Tiles {
-		d, _ := TileDescriptors[tile]
-		tileVector[d.Pos] = count
+	tileVector := make([]int, len(TileDescriptors))
+	for i, d := range TileDescriptors {
+		count, has := t.Tiles[d.Tile]
+		if !has {
+			count = 0
+		}
+		tileVector[i] = count
 	}
 	return tileVector
 }
