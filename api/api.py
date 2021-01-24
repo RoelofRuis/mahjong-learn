342a216b12d226b34d81fe92a0c924291cbd1f59
RoelofRuis <roelof.ruis@gmail.com>
2021-01-24 15:51:04 +0100

    trying to post some data

--- api/api.py
+++ api/api.py
@@ -1,9 +1,9 @@
 from urllib import request, parse
 
-data = {1: 1}
+data = {"1": 1}
 data = parse.urlencode(data).encode()
 
-req = request.Request("http://localhost:8000/action/1", data=data)
+req = request.Request("http://localhost:8000/action/1", method="POST", data=data)
 resp = request.urlopen(req)
 
 print(resp)
