9f7bce3bb3fff1b035e5f39da41fadf40a2659a4
RoelofRuis <roelof.ruis@gmail.com>
2021-01-28 18:10:01 +0100

    allow to wait until next move

--- api/naive_gameplay.py
+++ api/naive_gameplay.py
@@ -1,7 +1,11 @@
 import api
 
+# set to false to let the game run until ended
+WAIT_FOR_KEY = True
+
 # start new game
 id = api.new_game()["id"]
+print(f"game: {id}")
 
 while True:
     game_state = api.get_state(id)
@@ -11,10 +15,16 @@ while True:
     if has_ended:
         break
 
-    print(f"{state_name}: {active_player}")
+    print(f"State: {state_name} (player {active_player} to move)")
+
+    if WAIT_FOR_KEY:
+        input("Press enter to execute next move")
 
     # pick the first action for player 1
     api.send_actions(id, {f"{active_player}": 0})
+
+    if WAIT_FOR_KEY:
+        input("Press enter to execute next move")
 
     # pick the first actions for other players
     api.send_actions(id, {f"{p}": 0 for p in [1, 2, 3, 4] if p != active_player})
