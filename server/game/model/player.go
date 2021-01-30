ffb37c37947aee03f1959812541f5d9a31798ead
RoelofRuis <roelof.ruis@gmail.com>
2021-01-30 19:22:26 +0100

    move scoring to different file

--- server/game/model/player.go
+++ server/game/model/player.go
@@ -37,39 +37,6 @@ func (p *Player) GetDiscardedTiles() *TileCollection {
 	return p.discarded
 }
 
-func (p *Player) CanPung(t Tile) bool {
-	return p.concealed.NumOf(t) == 2
-}
-
-func (p *Player) CanKong(t Tile) bool {
-	return p.concealed.NumOf(t) == 3
-}
-
-func (p *Player) PossibleDiscards() []Tile {
-	var l []Tile
-	for t, c := range p.concealed.tiles {
-		if c > 0 {
-			l = append(l, t)
-		}
-	}
-	return l
-}
-
-func (p *Player) PossibleHiddenKongs() []Tile {
-	var l []Tile
-	for t, c := range p.concealed.tiles {
-		if c == 4 {
-			l = append(l, t)
-		}
-	}
-	return l
-}
-
-func (p *Player) CanDeclareMahjong() bool {
-	// TODO: implement
-	return false
-}
-
 // State modifiers
 
 func (p *Player) ForceExposeTiles() int {
